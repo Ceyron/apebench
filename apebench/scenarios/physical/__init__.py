b553eca9bf222f83577b56963961e4fef0fff28b
Felix Koehler <f.koehler@tum.de>
2024-05-22 10:48:15 +0200

    Implement linear scenarios in physical mode

--- apebench/scenarios/physical/__init__.py
+++ apebench/scenarios/physical/__init__.py
@@ -1,4 +1,14 @@
 from ._gray_scott import GrayScott, GrayScottType
+from ._linear import (
+    Advection,
+    AdvectionDiffusion,
+    Diffusion,
+    Dispersion,
+    FirstFour,
+    HyperDiffusion,
+    Linear,
+    LinearSimple,
+)
 from ._navier_stokes import DecayingTurbulence, KolmogorovFlow
 from ._poisson import Poisson
 from ._swift_hohenberg import SwiftHohenberg
@@ -10,4 +20,12 @@ scenario_dict = {
     "phy_gs_type": GrayScottType,
     "phy_decay_turb": DecayingTurbulence,
     "phy_kolm_flow": KolmogorovFlow,
+    "phy_lin": Linear,
+    "phy_lin_simple": LinearSimple,
+    "phy_adv": Advection,
+    "phy_diff": Diffusion,
+    "phy_adv_diff": AdvectionDiffusion,
+    "phy_disp": Dispersion,
+    "phy_hyp_diff": HyperDiffusion,
+    "phy_four": FirstFour,
 }
