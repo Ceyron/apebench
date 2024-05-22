11fe05d293d33e8aa75e8dbc7d904bb1298440b9
Felix Koehler <f.koehler@tum.de>
2024-05-22 09:17:06 +0200

    Adjust difficulties and names

--- apebench/scenarios/difficulty/_linear.py
+++ apebench/scenarios/difficulty/_linear.py
@@ -3,10 +3,7 @@ from ...exponax import exponax as ex
 
 
 class Linear(BaseScenario):
-    gammas: tuple[float, ...] = (
-        0.0,
-        -4.0,
-    )
+    gammas: tuple[float, ...] = (0.0, -4.0, 0.0, 0.0, 0.0)
     coarse_proportion: float = 0.5
 
     def get_ref_stepper(self):
@@ -32,77 +29,77 @@ class Linear(BaseScenario):
 
 
 class LinearSimple(Linear):
-    diff: float = -1.0
-    term_order: int = 1
+    linear_gamma: float = -4.0
+    linear_term_order: int = 1
 
     def __post_init__(self):
-        self.gammas = (0.0,) * self.term_order + (self.diff,)
+        self.gammas = (0.0,) * self.linear_term_order + (self.linear_gamma,)
 
 
 class Advection(Linear):
-    adv_difficulty: float = 4.0
+    advection_gamma: float = -4.0
 
     def __post_init__(self):
-        self.gammas = (0.0, -self.adv_difficulty)
+        self.gammas = (0.0, self.advection_gamma, 0.0, 0.0, 0.0)
 
     def get_scenario_name(self) -> str:
         return f"{self.num_spatial_dims}d_diff_adv"
 
 
 class Diffusion(Linear):
-    diff_difficulty: float = 1.0
+    diffusion_gamma: float = 4.0
 
     def __post_init__(self):
-        self.gammas = (0.0, 0.0, self.diff_difficulty)
+        self.gammas = (0.0, 0.0, self.diffusion_gamma, 0.0, 0.0)
 
     def get_scenario_name(self) -> str:
         return f"{self.num_spatial_dims}d_diff_diff"
 
 
 class AdvectionDiffusion(Linear):
-    adv_difficulty: float = 4.0
-    diff_difficulty: float = 1.0
+    advection_gamma: float = -4.0
+    diffusion_gamma: float = 4.0
 
     def __post_init__(self):
-        self.gammas = (0.0, -self.adv_difficulty, self.diff_difficulty)
+        self.gammas = (0.0, self.advection_gamma, self.diffusion_gamma, 0.0, 0.0)
 
     def get_scenario_name(self) -> str:
         return f"{self.num_spatial_dims}d_diff_adv_diff"
 
 
 class Dispersion(Linear):
-    disp_difficulty: float = 1.0
+    dispersion_gamma: float = 4.0
 
     def __post_init__(self):
-        self.gammas = (0.0, 0.0, 0.0, self.disp_difficulty)
+        self.gammas = (0.0, 0.0, 0.0, self.dispersion_gamma, 0.0)
 
     def get_scenario_name(self) -> str:
         return f"{self.num_spatial_dims}d_diff_disp"
 
 
 class HyperDiffusion(Linear):
-    hyp_difficulty: float = 1.0
+    hyp_diffusion_gamma: float = -4.0
 
     def __post_init__(self):
-        self.gammas = (0.0, 0.0, 0.0, 0.0, -self.hyp_difficulty)
+        self.gammas = (0.0, 0.0, 0.0, 0.0, self.hyp_diffusion_gamma, 0.0)
 
     def get_scenario_name(self) -> str:
-        return f"{self.num_spatial_dims}d_diff_hypdiff"
+        return f"{self.num_spatial_dims}d_diff_hyp_diff"
 
 
 class FirstFour(Linear):
-    adv_difficulty: float = 4.0
-    diff_difficulty: float = 1.0
-    disp_difficulty: float = 1.0
-    hyp_difficulty: float = 1.0
+    advection_gamma: float = -4.0
+    diffusion_gamma: float = 4.0
+    dispersion_gamma: float = 4.0
+    hyp_diffusion_gamma: float = -4.0
 
     def __post_init__(self):
         self.gammas = (
             0.0,
-            -self.adv_difficulty,
-            self.diff_difficulty,
-            self.disp_difficulty,
-            -self.hyp_difficulty,
+            self.advection_gamma,
+            self.diffusion_gamma,
+            self.dispersion_gamma,
+            self.hyp_diffusion_gamma,
         )
 
     def get_scenario_name(self) -> str:
