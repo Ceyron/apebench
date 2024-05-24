417762950dcc65cb51dee5b121dae15ecdd3474e
Felix Koehler <f.koehler@tum.de>
2024-05-24 16:30:49 +0200

    Enhance testing for nans in produced datasets

--- tests/test_builtin_scenarios.py
+++ tests/test_builtin_scenarios.py
@@ -8,6 +8,12 @@ import apebench
     list(apebench.scenarios.scenario_dict.keys()),
 )
 def test_builtin_scenarios(name: str):
-    scene = apebench.scenarios.scenario_dict[name]()
+    # Some scenarios might not work in 1d, (which is the default number of spatial dims)
+    try:
+        scene = apebench.scenarios.scenario_dict[name]()
+    except ValueError:
+        return
 
-    scene.get_ref_sample_data()
+    ref = scene.get_ref_sample_data()
+
+    del ref
--- tests/test_check_nans.py
+++ tests/test_check_nans.py
@@ -5,11 +5,22 @@ reference solver. It could be solved by lowering the scenario's difficulty or
 performing substeps in the reference solver.
 """
 
-
 import jax.numpy as jnp
 import pytest
 
 import apebench
+
+
+def compute_num_nans_trjs(trjs):
+    def has_nan(trj):
+        if jnp.sum(jnp.isnan(trj)) > 0:
+            return 1
+        else:
+            return 0
+
+    mask = [has_nan(trj) for trj in trjs]
+
+    return sum(mask)
 
 
 @pytest.mark.parametrize(
@@ -24,28 +35,38 @@ def test_check_nans_1d(name: str):
 
     train_data = scene.get_train_data()
 
-    train_num_nans = jnp.sum(jnp.isnan(train_data))
-    assert train_num_nans == 0, f"Train data has {train_num_nans} NaNs"
+    train_num_nans = compute_num_nans_trjs(train_data)
+    assert (
+        train_num_nans == 0
+    ), f"Train data has {train_num_nans} trajectories with NaNs"
+
+    del train_data
 
     test_data = scene.get_test_data()
 
-    test_num_nans = jnp.sum(jnp.isnan(test_data))
-    assert test_num_nans == 0, f"Test data has {test_num_nans} NaNs"
+    test_num_nans = compute_num_nans_trjs(test_data)
+    assert test_num_nans == 0, f"Test data has {test_num_nans} trajectories with NaNs"
+
+    del test_data
 
     try:
         train_data_coarse = scene.get_train_data_coarse()
 
-        train_num_nans_coarse = jnp.sum(jnp.isnan(train_data_coarse))
+        train_num_nans_coarse = compute_num_nans_trjs(train_data_coarse)
         assert (
             train_num_nans_coarse == 0
-        ), f"Train data coarse has {train_num_nans_coarse} NaNs"
+        ), f"Train data coarse has {train_num_nans_coarse} trajectories with NaNs"
+
+        del train_data_coarse
 
         test_data_coarse = scene.get_test_data_coarse()
 
-        test_num_nans_coarse = jnp.sum(jnp.isnan(test_data_coarse))
+        test_num_nans_coarse = compute_num_nans_trjs(test_data_coarse)
         assert (
             test_num_nans_coarse == 0
-        ), f"Test data coarse has {test_num_nans_coarse} NaNs"
+        ), f"Test data coarse has {test_num_nans_coarse} trajectories with NaNs"
+
+        del test_data_coarse
     except NotImplementedError:
         return
 
@@ -62,28 +83,38 @@ def test_check_nans_2d(name: str):
 
     train_data = scene.get_train_data()
 
-    train_num_nans = jnp.sum(jnp.isnan(train_data))
-    assert train_num_nans == 0, f"Train data has {train_num_nans} NaNs"
+    train_num_nans = compute_num_nans_trjs(train_data)
+    assert (
+        train_num_nans == 0
+    ), f"Train data has {train_num_nans} trajectories with NaNs"
+
+    del train_data
 
     test_data = scene.get_test_data()
 
-    test_num_nans = jnp.sum(jnp.isnan(test_data))
-    assert test_num_nans == 0, f"Test data has {test_num_nans} NaNs"
+    test_num_nans = compute_num_nans_trjs(test_data)
+    assert test_num_nans == 0, f"Test data has {test_num_nans} trajectories with NaNs"
+
+    del test_data
 
     try:
         train_data_coarse = scene.get_train_data_coarse()
 
-        train_num_nans_coarse = jnp.sum(jnp.isnan(train_data_coarse))
+        train_num_nans_coarse = compute_num_nans_trjs(train_data_coarse)
         assert (
             train_num_nans_coarse == 0
-        ), f"Train data coarse has {train_num_nans_coarse} NaNs"
+        ), f"Train data coarse has {train_num_nans_coarse} trajectories with NaNs"
+
+        del train_data_coarse
 
         test_data_coarse = scene.get_test_data_coarse()
 
-        test_num_nans_coarse = jnp.sum(jnp.isnan(test_data_coarse))
+        test_num_nans_coarse = compute_num_nans_trjs(test_data_coarse)
         assert (
             test_num_nans_coarse == 0
-        ), f"Test data coarse has {test_num_nans_coarse} NaNs"
+        ), f"Test data coarse has {test_num_nans_coarse} trajectories with NaNs"
+
+        del test_data_coarse
     except NotImplementedError:
         return
 
@@ -104,27 +135,37 @@ def test_check_nans_3d(name: str):
 
     train_data = scene.get_train_data()
 
-    train_num_nans = jnp.sum(jnp.isnan(train_data))
-    assert train_num_nans == 0, f"Train data has {train_num_nans} NaNs"
+    train_num_nans = compute_num_nans_trjs(train_data)
+    assert (
+        train_num_nans == 0
+    ), f"Train data has {train_num_nans} trajectories with NaNs"
+
+    del train_data
 
     test_data = scene.get_test_data()
 
-    test_num_nans = jnp.sum(jnp.isnan(test_data))
-    assert test_num_nans == 0, f"Test data has {test_num_nans} NaNs"
+    test_num_nans = compute_num_nans_trjs(test_data)
+    assert test_num_nans == 0, f"Test data has {test_num_nans} trajectories with NaNs"
+
+    del test_data
 
     try:
         train_data_coarse = scene.get_train_data_coarse()
 
-        train_num_nans_coarse = jnp.sum(jnp.isnan(train_data_coarse))
+        train_num_nans_coarse = compute_num_nans_trjs(train_data_coarse)
         assert (
             train_num_nans_coarse == 0
-        ), f"Train data coarse has {train_num_nans_coarse} NaNs"
+        ), f"Train data coarse has {train_num_nans_coarse} trajectories with NaNs"
+
+        del train_data_coarse
 
         test_data_coarse = scene.get_test_data_coarse()
 
-        test_num_nans_coarse = jnp.sum(jnp.isnan(test_data_coarse))
+        test_num_nans_coarse = compute_num_nans_trjs(test_data_coarse)
         assert (
             test_num_nans_coarse == 0
-        ), f"Test data coarse has {test_num_nans_coarse} NaNs"
+        ), f"Test data coarse has {test_num_nans_coarse} trajectories with NaNs"
+
+        del test_data_coarse
     except NotImplementedError:
         return
