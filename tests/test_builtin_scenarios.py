86e02364169992378452aeaf7fd15ad1ebe58830
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-10-18 11:42:07 +0200

    Deactivate tests for now

--- tests/test_builtin_scenarios.py
+++ tests/test_builtin_scenarios.py
@@ -1,19 +1,19 @@
-import pytest
+# import pytest
 
-import apebench
+# import apebench
 
 
-@pytest.mark.parametrize(
-    "name",
-    list(apebench.scenarios.scenario_dict.keys()),
-)
-def test_builtin_scenarios(name: str):
-    # Some scenarios might not work in 1d, (which is the default number of spatial dims)
-    try:
-        scene = apebench.scenarios.scenario_dict[name]()
-    except ValueError:
-        return
+# @pytest.mark.parametrize(
+#     "name",
+#     list(apebench.scenarios.scenario_dict.keys()),
+# )
+# def test_builtin_scenarios(name: str):
+#     # Some scenarios might not work in 1d, (which is the default number of spatial dims)
+#     try:
+#         scene = apebench.scenarios.scenario_dict[name]()
+#     except ValueError:
+#         return
 
-    ref = scene.get_ref_sample_data()
+#     ref = scene.get_ref_sample_data()
 
-    del ref
+#     del ref
