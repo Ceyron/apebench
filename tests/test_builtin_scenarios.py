7c130364ac2a914b2ec3f3fd515124eb958b4de0
Felix Koehler <f.koehler@tum.de>
2024-10-23 10:13:11 +0200

    Add test that runs a simple training

--- tests/test_builtin_scenarios.py
+++ tests/test_builtin_scenarios.py
@@ -21,3 +21,31 @@ def test_builtin_scenarios(name: str):
     ref = scene.get_ref_sample_data()
 
     del ref
+
+
+@pytest.mark.parametrize(
+    "name",
+    list(apebench.scenarios.scenario_dict.keys()),
+)
+def test_simple_training_1d(name: str):
+    NUM_TRAIN_SAMPLES = 5
+    NUM_TEST_SAMPLES = 5
+    OPTIM_CONFIG = "adam;10;constant;1e-4"
+
+    # Some scenarios might not work in 1d, (which is the default number of spatial dims)
+    try:
+        scene = apebench.scenarios.scenario_dict[name](
+            num_train_samples=NUM_TRAIN_SAMPLES,
+            num_test_samples=NUM_TEST_SAMPLES,
+            optim_config=OPTIM_CONFIG,
+        )
+    except ValueError:
+        return
+
+    NETWORK_CONFIG = "Conv;10;2;relu"
+
+    data, trained_net = scene(
+        network_config=NETWORK_CONFIG,
+    )
+
+    del data, trained_net
