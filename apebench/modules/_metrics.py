f5211c076a07630929d636ea32320ffeb3042d37
Felix Koehler <f.koehler@tum.de>
2024-10-23 11:57:50 +0200

    Also have fourier metrics defined inline

--- apebench/modules/_metrics.py
+++ apebench/modules/_metrics.py
@@ -3,26 +3,30 @@ from typing import Callable, Dict
 import exponax as ex
 from jaxtyping import Array, Float
 
+# def mean_fourier_nRMSE_constructor(
+#     metric_config: str,
+# ) -> Callable[
+#     [Array[Float, "batch channel ... N"], Array[Float, "batch channel ... N"]], float
+# ]:
+#     metric_args = metric_config.split(";")
+#     low = int(metric_args[1])
+#     high = int(metric_args[2])
+#     if len(metric_args) > 3:
+#         derivative_order = float(metric_args[3])
+#     else:
+#         derivative_order = None
 
-def mean_fourier_nRMSE_constructor(
-    metric_config: str,
-) -> Callable[
-    [Array[Float, "batch channel ... N"], Array[Float, "batch channel ... N"]], float
-]:
-    metric_args = metric_config.split(";")
-    low = int(metric_args[1])
-    high = int(metric_args[2])
+#     def mean_fourier_nRMSE(pred, ref):
+#         return ex.metrics.mean_metric(
+#             ex.metrics.fourier_nRMSE,
+#             pred,
+#             ref,
+#             low=low,
+#             high=high,
+#             derivative_order=derivative_order,
+#         )
 
-    def mean_fourier_nRMSE(pred, ref):
-        return ex.metrics.mean_metric(
-            ex.metrics.fourier_nRMSE,
-            pred,
-            ref,
-            low=low,
-            high=high,
-        )
-
-    return mean_fourier_nRMSE
+#     return mean_fourier_nRMSE
 
 
 metrics_dict: Dict[
@@ -70,5 +74,36 @@ metrics_dict: Dict[
         pred,
         ref,
     ),
-    "mean_fourier_nRMSE": mean_fourier_nRMSE_constructor,
+    "mean_fourier_MSE": lambda metric_config: lambda pred, ref: ex.metrics.mean_metric(
+        ex.metrics.fourier_MSE,
+        pred,
+        ref,
+        low=int(metric_config.split(";")[1]),
+        high=int(metric_config.split(";")[2]),
+        derivative_order=float(metric_config.split(";")[3]),
+    ),
+    "mean_fourier_nMSE": lambda metric_config: lambda pred, ref: ex.metrics.mean_metric(
+        ex.metrics.fourier_nMSE,
+        pred,
+        ref,
+        low=int(metric_config.split(";")[1]),
+        high=int(metric_config.split(";")[2]),
+        derivative_order=float(metric_config.split(";")[3]),
+    ),
+    "mean_fourier_RMSE": lambda metric_config: lambda pred, ref: ex.metrics.mean_metric(
+        ex.metrics.fourier_RMSE,
+        pred,
+        ref,
+        low=int(metric_config.split(";")[1]),
+        high=int(metric_config.split(";")[2]),
+        derivative_order=float(metric_config.split(";")[3]),
+    ),
+    "mean_fourier_nRMSE": lambda metric_config: lambda pred, ref: ex.metrics.mean_metric(
+        ex.metrics.fourier_nRMSE,
+        pred,
+        ref,
+        low=int(metric_config.split(";")[1]),
+        high=int(metric_config.split(";")[2]),
+        derivative_order=float(metric_config.split(";")[3]),
+    ),
 }
