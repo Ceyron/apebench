938f559f9484e4a158e4fbd54a36d960ab36bf83
Felix Koehler <f.koehler@tum.de>
2024-10-23 10:40:58 +0200

    Also test in higher dimensions

--- tests/test_builtin_scenarios.py
+++ tests/test_builtin_scenarios.py
@@ -24,23 +24,30 @@ def test_builtin_scenarios(name: str):
 
 
 @pytest.mark.parametrize(
-    "name",
+    "name,num_spatial_dims",
     [
-        "phy_adv",
-        "norm_adv",
-        "diff_adv",
+        (name, num_spatial_dims)
+        for name in [
+            "phy_adv",
+            "norm_adv",
+            "diff_adv",
+        ]
+        for num_spatial_dims in [1, 2, 3]
     ],
 )
-def test_simple_training_1d(name: str):
+def test_simple_training(name: str, num_spatial_dims: int):
     NUM_TRAIN_SAMPLES = 5
     NUM_TEST_SAMPLES = 5
+    NUM_POINTS = 40
     OPTIM_CONFIG = "adam;10;constant;1e-4"
 
     # Some scenarios might not work in 1d, (which is the default number of spatial dims)
     try:
         scene = apebench.scenarios.scenario_dict[name](
+            num_spatial_dims=num_spatial_dims,
             num_train_samples=NUM_TRAIN_SAMPLES,
             num_test_samples=NUM_TEST_SAMPLES,
+            num_points=NUM_POINTS,
             optim_config=OPTIM_CONFIG,
         )
     except ValueError:
