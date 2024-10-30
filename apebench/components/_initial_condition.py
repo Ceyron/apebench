6b303c189ece69df37db55a556eb9c251893500d
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-10-30 12:25:02 +0100

    Fix ic components (#24)
    * Fix offset usage for Fourier init
    
    * Fix using correct index

--- apebench/components/_initial_condition.py
+++ apebench/components/_initial_condition.py
@@ -15,9 +15,9 @@ ic_dict: Dict[
     "fourier": lambda ic_config, num_spatial_dims: ex.ic.RandomTruncatedFourierSeries(
         num_spatial_dims=num_spatial_dims,
         cutoff=int(ic_config.split(";")[1]),
-        offset_range=(-0.5, 0.5)
+        offset_range=(0.0, 0.0)
         if ic_config.split(";")[2].lower() == "true"
-        else (0, 0),
+        else (-0.5, 0.5),
         max_one=ic_config.split(";")[3].lower() == "true",
     ),
     "diffused": lambda ic_config, num_spatial_dims: ex.ic.DiffusedNoise(
@@ -29,7 +29,7 @@ ic_dict: Dict[
     "grf": lambda ic_config, num_spatial_dims: ex.ic.GaussianRandomField(
         num_spatial_dims=num_spatial_dims,
         powerlaw_exponent=float(ic_config.split(";")[1]),
-        zero_mean=ic_config.split(";")[3].lower() == "true",
+        zero_mean=ic_config.split(";")[2].lower() == "true",
         max_one=ic_config.split(";")[3].lower() == "true",
     ),
 }
